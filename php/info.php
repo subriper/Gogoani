5363909190f396527a10eb332fd5d87470c437c8
subriper <156682105+subriper@users.noreply.github.com>
2026-05-02 03:23:00 +0330

    Update info.php

--- php/info.php
+++ php/info.php
@@ -1,5 +1,5 @@
 <?php 
-$base_url = "https://gogoani6.vercel.app/";
+$base_url = "https://gogoani6.vercel.app";
 $website_name = "GogoAnime";
 $apiLink = "https://apian-flame.vercel.app/"; 
 $consumet = "https://api.consumet.org/"; 
