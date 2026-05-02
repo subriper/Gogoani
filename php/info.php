3b8aa597757657575d6fa4fcd7b9ff39ef9acbdf
subriper <156682105+subriper@users.noreply.github.com>
2026-05-02 03:51:44 +0330

    Update info.php

--- php/info.php
+++ php/info.php
@@ -1,6 +1,7 @@
 <?php 
 $base_url = "https://gogoani6.vercel.app";
 $website_name = "GogoAnime";
-$apiLink = "https://apian-flame.vercel.app/"; 
+// اسلش آخر آدرس زیر را حذف کردم
+$apiLink = "https://apian-flame.vercel.app"; 
 $consumet = "https://api.consumet.org/"; 
 ?>
